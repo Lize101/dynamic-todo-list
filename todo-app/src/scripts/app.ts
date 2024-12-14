2bcea9a05af78a8e3926c92a26b753654f4cd821
Lizdigital <lizetteclaireh@gmail.com>
2024-12-14 11:01:27 +0000

    Add form functionality - add todo list items to the list - use of TypeScript

---
+++ todo-app/src/scripts/app.ts
@@ -0,0 +1,19 @@
+// const message: string = 'Hello You';
+// console.log(message);
+
+const form = document.getElementById('form') as HTMLFormElement;
+const input = document.getElementById('input') as HTMLInputElement;
+// const btn = document.getElementById('btn');
+const list = document.getElementById('list') as HTMLUListElement;
+
+form.addEventListener('submit', function(e: Event){
+    e.preventDefault();
+    let text = input.value.trim();
+    if(!text) return;
+
+    const li = document.createElement('li');
+    li.textContent = text;
+    list.appendChild(li);
+    input.value = '';
+
+})
