82b74195f83173ee2386f0262eeb89d22bebca2d
Lizdigital <lizetteclaireh@gmail.com>
2025-01-09 17:56:55 +0000

    Add more test to validate input

--- todo-app/tests/utils.test.ts
+++ todo-app/tests/utils.test.ts
@@ -11,3 +11,11 @@ test('if input value has whitespace then return false', () => {
 test('if input value is valid then return true', () => {
     expect(checkInput('Item')).toBe(true);
 });
+
+test('if spaces surround the value then trim and return true', () => {
+    expect(checkInput(' Item2 ')).toBe(true);
+});
+
+test('if special characters exist then return true', () => {
+    expect(checkInput('!&*%£')).toBe(true);
+});
